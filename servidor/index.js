4e3971fd398cbcde38c1769f0c59c43271a58f1e
Fernando Icardi <fernando.icardi@gmail.com>
2023-08-02 20:15:33 -0300

    se crea servidor con una ruta que devuelve "Hola mundo"

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,13 @@
-console.log("hola soy servidor");
+const express = require('express');
+
+//creamos el servidor
+const app = express();
+
+//definimos ruta principal
+app.get('/',(req,res)=> {
+    res.send('Hola Mundo');
+})
+
+app.listen(4000,()=>{
+    console.log('El servidor esta corriendo perfectamente');
+})
