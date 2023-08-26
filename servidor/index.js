e8fc3ac9095c6cb2e3d6764d380c48c199c2865d
Fernando Icardi <fernando.icardi@gmail.com>
2023-08-25 21:47:34 -0300

    configuracion routers para llamados desde postman al servidor

--- servidor/index.js
+++ servidor/index.js
@@ -7,10 +7,7 @@ const app = express();
 // conectamos a la base de datos
 conectarDB();
 
-//definimos ruta principal
-app.get('/',(req,res)=> {
-    res.send('Hola Mundo');
-})
+app.use('/api/productos', require('./routes/producto'));
 
 app.listen(4000,()=>{
     console.log('El servidor esta corriendo perfectamente');
