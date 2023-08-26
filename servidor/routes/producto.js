5848a0be78a3a84985e3713b8080c0ffa76a6d23
Fernando Icardi <fernando.icardi@gmail.com>
2023-08-25 21:56:31 -0300

    controlador producto creado para crear un producto

--- servidor/routes/producto.js
+++ servidor/routes/producto.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const productoController = require('../controllers/productoController');
+
 
 //api/productos
-router.post('/', () => {
-    console.log('Creando producto');
-});
+router.post('/',productoController.crearProducto);
 
 module.exports = router;
