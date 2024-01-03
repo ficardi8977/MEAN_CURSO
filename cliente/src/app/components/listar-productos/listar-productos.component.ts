32fd66f6c39cfe94de66ed3bc51a7dd2bba092e0
Fernando Icardi <fernando.icardi@gmail.com>
2024-01-02 21:41:45 -0300

    se sube cambios de listado de productos

--- cliente/src/app/components/listar-productos/listar-productos.component.ts
+++ cliente/src/app/components/listar-productos/listar-productos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductoService } from 'src/app/services/producto.service';
+import { producto } from 'src/app/models/producto/producto';
 
 @Component({
   selector: 'app-listar-productos',
@@ -8,6 +9,7 @@ import { ProductoService } from 'src/app/services/producto.service';
 })
 export class ListarProductosComponent {
 
+  listaProductos : producto[] = []
   // inyectamos el producto service para poder llamar al backend
   constructor(private _productoService : ProductoService){}
 
@@ -18,6 +20,7 @@ export class ListarProductosComponent {
   obtenerProductos(){
     this._productoService.getProductos().subscribe(data => {
       console.log(data);
+      this.listaProductos = data;
     },error => {
       console.log(error)
     })
